9c78c48af40ef266a96d6d52ee8403bfc34ed0d5
Loïc <l.vandenbossche.jouvet@gmail.com>
2022-01-22 19:50:21 +0100

    Fixed env loading for local mode and add npm scripts

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -11,20 +11,26 @@ import { RolesModule } from './roles/role.module';
 import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ["dist/**/*.model{.ts,.js}"],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: ["dist/**/*.model{.ts,.js}"],
+        synchronize: true,
+      })
     }),
     RolesModule,
-
+    ConfigModule.forRoot({
+      envFilePath: '../../.env'
+    }),
     UserModule,
     AuthModule,
     PassportModule,
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { loadEnvironmentVariables } from './utils/env';
 import { Logger } from './utils/logger';
 
-loadEnvironmentVariables();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: new Logger });
   const port = Number(process.env.BACKEND_PORT) || 4000;
