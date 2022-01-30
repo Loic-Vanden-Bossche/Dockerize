5b1b485718e42afee8d46fdb79c89b023cdfe5c8
Loïc <l.vandenbossche.jouvet@gmail.com>
2022-01-30 14:12:22 +0100

    Fixed logger for test env

--- packages/backend/src/utils/logger.ts
+++ packages/backend/src/utils/logger.ts
@@ -1,11 +1,41 @@
 import { Module } from '@nestjs/common';
-import { Injectable, Scope, ConsoleLogger } from '@nestjs/common';
+import { Injectable, Scope, ConsoleLogger, Logger as BaseLogger } from '@nestjs/common';
 
-export const silent = (): boolean =>
-  process.env.NODE_ENV === 'test' && process.env.VERBOSE_TESTS !== 'true';
+export const silent = (): boolean => process.env.NODE_ENV === 'test';
 
 @Injectable({ scope: Scope.TRANSIENT })
-export class Logger extends ConsoleLogger {}
+export class Logger extends ConsoleLogger {
+  private static _log(
+    level: string,
+    message: unknown,
+    ctx?: string,
+    trace?: string,
+  ): void {
+    try {
+      if (!silent()) {
+        BaseLogger[level](message, trace ? trace : ctx, trace ? ctx : null);
+      }
+    } catch (e) {
+      BaseLogger.error(e.message, e.trace, 'logger');
+    }
+  }
+
+  log(message: unknown, ctx?: string): void {
+    Logger._log('log', message, ctx);
+  }
+  error(message: unknown, trace?: string, ctx?: string): void {
+    Logger._log('error', message, ctx, trace);
+  }
+  warn(message: unknown, ctx?: string): void {
+    Logger._log('warn', message, ctx);
+  }
+  debug(message: unknown, ctx?: string): void {
+    Logger._log('debug', message, ctx);
+  }
+  verbose(message: unknown, ctx?: string): void {
+    Logger._log('verbose', message, ctx);
+  }
+}
 
 @Module({
   providers: [Logger],
