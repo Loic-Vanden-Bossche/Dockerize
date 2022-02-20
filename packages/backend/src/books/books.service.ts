a29fd3b44334eaae4922220619b2516cea8be561
Loïc <l.vandenbossche.jouvet@gmail.com>
2022-02-20 17:27:32 +0100

    Back: added book search and overview resolver in add-book

--- packages/backend/src/books/books.service.ts
+++ packages/backend/src/books/books.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PaginatorService } from '../common/paginator/paginator.service';
 import { IPageOptions } from '../common/paginator/dto/page-options.interface';
 import { Page } from '../common/paginator/dto/page.dto';
 import { Book } from './book.model';
+import { firstValueFrom } from "rxjs";
 
 @Injectable()
 export class BooksService {
@@ -12,6 +14,7 @@ export class BooksService {
     @InjectRepository(Book)
     private readonly bookRepository: Repository<Book>,
     private readonly paginatorService: PaginatorService,
+    private readonly http: HttpService
   ) {}
 
   async findAll(options?: IPageOptions<Book>): Promise<Page<Book>> {
@@ -20,6 +23,26 @@ export class BooksService {
       options,
       this.bookRepository,
     );
+  }
+
+  async search(query) {
+
+    const checkArray = (arr) => Array.isArray(arr) && arr?.length;
+
+    return firstValueFrom(this.http.get(`http://openlibrary.org/search.json?title=${query}`)).then(res => {
+      return res.data.docs.filter(book => checkArray(book.isbn) && checkArray(book.author_name)).map(book => ({
+        isbn: book.isbn.filter(isbn => isbn.length === 13)[0],
+        title: book.title,
+        author: book.author_name[0]
+      })).map(book => ({...book, picture: `https://covers.openlibrary.org/b/isbn/${book.isbn}-L.jpg`}))
+    })
+  }
+
+  async getOverviewFromIsbn(isbn: string) {
+    return firstValueFrom(this.http.get(`https://openlibrary.org/isbn/${isbn}.json`))
+      .then(book => book.data.works[0].key.split('/')[2])
+      .then(key => firstValueFrom(this.http.get(`https://openlibrary.org/works/${key}.json`)))
+      .then(data => data.data.description.value.slice(0, 1500))
   }
 
   async findByIsbn(isbn: string): Promise<Book> {
