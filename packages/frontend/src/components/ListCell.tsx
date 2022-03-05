7f3bc0e108329df603ddc3e0cd22d29c0a878090
Enzo SOARES <enzoayrsoares@gmail.com>
2022-03-05 09:53:20 +0100

    front: Fix book side

--- packages/frontend/src/components/ListCell.tsx
+++ packages/frontend/src/components/ListCell.tsx
@@ -6,7 +6,7 @@ const ListCell = (prop: {book: Book}) => {
             <div className="preview">
                 <div className="image">
                     <div className="slice">
-                        <img src={`https://images.isbndb.com/covers/53/15/${prop.book.isbn}.jpg`} alt={prop.book.title}/>
+                        <img src={prop.book.picture} alt={prop.book.title}/>
                     </div>
                     <img src={prop.book.picture} alt={prop.book.title}/>
                     <div className="shadow"/>
